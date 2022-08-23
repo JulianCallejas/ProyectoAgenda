c0fec09dad1e057dd3c1022185c6bdf30bc8d58c
a3cortes <a3cortes@gmail.com>
2022-08-23 13:02:15 -0500

    Update app.py

--- AgendApp/app.py
+++ AgendApp/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #Inicializamos flask con la constate name
 
 #Definimos la ruta principal cuando abrimos el servidor
 @app.route('/')
 def index():
-    return '     Hola Mundo !!!!!   '
+     return render_template("index.html")
 
 app.run(debug=True) # estamos en modo desarrollo
