c6d99f591b0edcaac62e25579f5df7efe15c8113
JulianCallejas <juliecallejas@hotmail.com>
2022-09-17 02:25:59 -0500

    Inicio de adminDashboard

--- AgendApp/controller/adminDashboard.py
+++ AgendApp/controller/adminDashboard.py
@@ -3,12 +3,22 @@ from flask_login import LoginManager, login_user, logout_user, login_required
 from model.ModelUser import ModelUser
 from model.persona import Persona
 
+
+
+def cargaUsuarios(db):
+        cursor=db.connection.cursor()
+        sql = """CALL SP_SelectAllUsuarios()"""
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        return data
+
+
 class SettingsController():
     @classmethod
     def loginController(rq, db, logged_user):
         if logged_user.esAdmin:
-            flash("PANTALLA ADMINISTRADOR")
-            return render_template('admin-dashboard.html')
+            data = cargaUsuarios(db)
+            return render_template('admin-dashboard.html', usuarios = data)
         else:
             flash("Debe ingresar como Administrador")
             return render_template('Login.html')
